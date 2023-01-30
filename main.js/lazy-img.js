cd66b9d726f92a68c5fdc5924b03c9c193a4ca22
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2023-01-30 13:59:51 +0200

    ...

--- main.js/lazy-img.js
+++ main.js/lazy-img.js
@@ -7,14 +7,3 @@ lazyImg.forEach((imeg) => {
 function onImageLoadet() {
   event.target.classList.add("appear");
 }
-
-// if ("loading" in HTMLImageElement.prototype) {
-// } else {
-//   const script = document.createElement("script");
-//   script.srs =
-//     "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
-//   script.integrity = "sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=";
-//   script.crossorigin = "anonymous";
-//   console.log("браузер не поддерживает ленивки");
-//   document.body.appendChild(script);
-// }
